706c88fa82fe5e46f6d7760e6e07af34e5b0a6a1
Armin <hihijo07@gmail.com>
2016-09-10 17:20:30 +0900

    [no-number]add instance Manager

---
+++ juggler/instanceManager.py
@@ -0,0 +1,12 @@
+from instanceMonitor import Monitor
+
+
+class instanceManager :
+    def RunManager(self) :
+        print("Instance Manage is activated")
+
+
+
+if __name__ == "__main__":
+    i = instanceManager()
+    i.RunManager()
