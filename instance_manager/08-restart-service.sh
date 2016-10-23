b42919be33058455b3bc7e94c362402fe5997277
Wangseok Lee <seok0721@gmail.com>
2016-10-23 12:12:24 +0900

    add restarting service script

---
+++ instance_manager/08-restart-service.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+service apache2 restart
+service glance-api restart
+service glance-registry restart
+service libvirt-bin restart
+service mysql restart
+service neutron-dhcp-agent restart
+service neutron-l3-agent restart
+service neutron-linuxbridge-agent restart
+service neutron-metadata-agent restart
+service neutron-server restart
+service nova-api restart
+service nova-conductor restart
+service nova-consoleauth restart
+service nova-novncproxy restart
+service nova-scheduler restart
+service rabbitmq-server restart
