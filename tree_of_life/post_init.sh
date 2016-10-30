a54f5dcb5d4610d51117ca03c1cb3daa4d066e57
Wangseok Lee <seok0721@gmail.com>
2016-10-31 03:47:56 +0900

    fix bug

--- tree_of_life/post_init.sh
+++ tree_of_life/post_init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo su -c "echo nameserver 8.8.8.8 > /etc/resolv.conf"
+sudo apt-get update
 sudo apt-get -y install python3-pip
 sudo pip3 install psutil pika
 git clone -b Release1.1 http://github.com/hoonkim/rune $HOME/rune
