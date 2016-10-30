184a9494df6bfdf7ade38624731cb585b6427851
Wangseok Lee <seok0721@gmail.com>
2016-10-31 02:56:01 +0900

    fix bug

--- tree_of_life/launch_instance.sh
+++ tree_of_life/launch_instance.sh
@@ -4,6 +4,10 @@ if [ -z $NUM ]; then NUM=1; fi
 openstack server create --flavor default --key-name default --nic net-id=private --image xenial --security-group default vm$NUM
 IP=`openstack floating ip create public -c floating_ip_address -f value`
 nova floating-ip-associate vm1 $IP
+while true; do
+  STATUS=`openstack server show vm$NUM -c status -f value`
+  if [ "$STATUS" == "ACTIVE" ]; then break; fi
+done
 scp post_init.sh ubuntu@$IP:.
 ssh ubuntu@$IP ./post_init.sh
 UUID=`openstack server show -c id -f value`
