447ea4f6741752418f07b5193a78b1d92cb89f55
Wangseok Lee <seok0721@gmail.com>
2016-11-06 21:29:59 +0900

    add locale-gen

--- instance_manager/controller/post_init.sh
+++ instance_manager/controller/post_init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+locale-gen en_US.UTF-8
+locale-gen ko_KR.UTF-8
 sudo su -c "echo nameserver 8.8.8.8 > /etc/resolv.conf"
 sudo apt-get update
 sudo apt-get -y install python3-pip rabbitmq-server
 sudo pip3 install psutil pika
 sudo service rabbitmq-server start
-git clone -b Release1.1 http://github.com/hoonkim/rune $HOME/rune
+git clone -b Release1.1 http://github.com/hoonkim/rune /home/ubuntu/rune
