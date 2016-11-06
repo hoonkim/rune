8ff47997d62e90d346846e566536dabf1ea56b86
Wangseok Lee <seok0721@gmail.com>
2016-11-06 23:34:40 +0900

    reconstitute script directory structure

--- tree_of_life/post_init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo su -c "echo nameserver 8.8.8.8 > /etc/resolv.conf"
-sudo apt-get update
-sudo apt-get -y install python3-pip rabbitmq-server
-sudo pip3 install psutil pika
-sudo service rabbitmq-server start
-git clone -b Release1.1 http://github.com/hoonkim/rune $HOME/rune
