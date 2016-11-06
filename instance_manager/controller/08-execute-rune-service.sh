397b28ca2ac8d0879659ecec4e2656766bbcbfe4
Wangseok Lee <seok0721@gmail.com>
2016-11-06 16:28:09 +0900

    add ubuntu 16.04 image download script

---
+++ instance_manager/controller/08-execute-rune-service.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source /etc/admin-openrc
+unset OS_TOKEN
+openstack token issue
+
+cd /rune
+wget https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.vmdk xenial.vmdk
+openstack image create xenial --container-format bare --disk-format vmdk --public
+
+git clone -b Release1.1 http://github.com/hoonkim/rune /rune
+echo "DROP DATABASE IF EXISTS rune_dev" | mysql
+echo "CREATE DATABASE rune_dev" | mysql
+cat /rune/schema/sentinel.sql | mysql rune_dev
+nohup python3 /rune/sentinel/run.py 9000 1>/var/log/sentinel.log 2>/var/log/sentinel-error.log &
+nohup python3 /rune/dashboard_demo/manage.py runserver 0.0.0.0:8080 1>/var/log/dashboard.log 2>/var/log/dashboard-error.log &
+echo "Rune services run successful"
