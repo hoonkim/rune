1a29ac81fae60cea4d8cd9866efe2442152a6c80
Wangseok Lee <seok0721@gmail.com>
2016-11-06 16:45:49 +0900

    update key name

--- instance_manager/controller/launch-new-instance.sh
+++ instance_manager/controller/launch-new-instance.sh
@@ -5,7 +5,7 @@ openstack token issue
 
 NUM=`openstack server list -c Name -f value | grep '^vm' | sort -r | head -n 1 | sed 's/vm//'`
 if [ -z $NUM ]; then NUM=1; fi
-openstack server create --flavor rune --key-name default --nic net-id=selfservice --image xenial --security-group default vm$NUM
+openstack server create --flavor rune --key-name mykey --nic net-id=selfservice --image xenial --security-group default vm$NUM
 IP=`openstack floating ip create provider -c floating_ip_address -f value`
 nova floating-ip-associate vm1 $IP
 while true; do
