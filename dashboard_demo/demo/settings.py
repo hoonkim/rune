db245856b406faa030f6df5db6da73759276207d
wongeun.song <wonggss@gmail.com>
2016-11-09 20:49:42 +0900

    code highlighting

--- dashboard_demo/demo/settings.py
+++ dashboard_demo/demo/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -121,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    "/home/wongeun/src/rune/dashboard_demo/webpage/templates/static",
+]
+
